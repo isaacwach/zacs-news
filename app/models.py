e42ce07d205fbb3f02f841fe2a3e1c32e563cd7e
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-25 21:16:51 +0300

    added source and headlines classes

--- app/models.py
+++ app/models.py
@@ -21,4 +21,25 @@ class Category:
         self.url = url
         self.image = image
         self.title = title  
-        
+
+class Source:
+    '''
+    Source class to define source objects
+    '''
+    def __init__(self,id,name,description,url):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+
+class Headlines:
+    '''
+    Class that instantiates objects of the headlines categories objects of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
