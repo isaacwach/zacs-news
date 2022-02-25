18816e632c8708decd52396a0163aeb9a5bd1383
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-25 20:32:57 +0300

    created general configuration parent class

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
 import os
+
+class Config:
+    '''
+    Creating configuration main class
+    '''
+    NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
+    # CAT_API_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
+    NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
+    CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
