1e56a7ba72cadc92b2f1b79e68e5923bdc828122
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-25 20:34:32 +0300

    created production and development configuration classes

--- config.py
+++ config.py
@@ -8,3 +8,21 @@ class Config:
     # CAT_API_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
     CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
