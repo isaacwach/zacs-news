9f875fd4ad2e629c0aa3056d67b9d5869c58af92
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-26 11:49:23 +0300

    initialized flask extensions, registered the main blueprint and imported a configure request

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,30 @@ from flask import Flask
 # from .config import DevConfig
 from flask_bootstrap import Bootstrap 
 from config import config_options
+
+# bootstrap instance
 bootstrap = Bootstrap()
 
 def create_app(config_name):
+
+    #flask app instance 
     app= Flask(__name__)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .request import configure_request
+    configure_request(app)
+
     return app
 
-from . import views
+#from . import views
     
