8f74c8c39dc362b76f5e025305c9c0d228db52d6
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-26 11:44:21 +0300

    created a bootstrap instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 # from .config import DevConfig
 from flask_bootstrap import Bootstrap 
+from config import config_options
+bootstrap = Bootstrap()
 
 def create_app(config_name):
     app= Flask(__name__)
