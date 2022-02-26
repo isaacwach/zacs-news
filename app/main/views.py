89ce02974290eced00e1c52aae88b111aae23d31
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-26 18:48:20 +0300

    declared source and headlines variables in index function

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,12 @@ from flask import render_template, request, redirect, url_for
 from . import main
 from ..request import get_source,article_source,get_category,get_headlines
 
-@app.route('/')
+@main.route('/')
 def index():
-    title= "Zacs trendy news app"
-    return render_template('index.html', title=title)
+    '''
+    View root page function that returns the index page and its data
+    '''
+    # title= "Zacs trendy news app"
+    source= get_source()
+    headlines = get_headlines()
+    return render_template('index.html', source=source, headlines=headlines)
