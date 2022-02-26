253640193905d9e61f9f6932799a2165f94bf2b4
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-26 22:45:30 +0300

    removed bugs

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     '''
     Creating configuration main class
     '''
-    NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?language=en&apiKey={}'
     # CAT_API_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
     CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
