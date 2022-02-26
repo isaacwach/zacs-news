7efeba230c5c9fea05089511fdabc82cb89a0ecf
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-26 16:26:12 +0300

    imported request, redirect, url_for from flask, main, and get_source, article_source, get_category and get headlines from reuests

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
-from app import app
+from flask import render_template, request, redirect, url_for
+from . import main
+from ..request import get_source,article_source,get_category,get_headlines
 
 @app.route('/')
 def index():
